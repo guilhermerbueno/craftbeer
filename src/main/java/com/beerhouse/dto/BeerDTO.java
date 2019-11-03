bcb7512c6e7da816a252c72f03f44362277c3bf5
guilhermerbueno <gbueno91@gmail.com>
2019-11-03 18:00:42 -0300

    Adding DTO to hidden persistent object

---
+++ src/main/java/com/beerhouse/dto/BeerDTO.java
@@ -0,0 +1,72 @@
+package com.beerhouse.dto;
+
+public class BeerDTO {
+    private Integer id;
+
+    private String name;
+
+    private String ingredients;
+
+    private String alcoholContent;
+
+    private Number price;
+
+    private String category;
+
+    public BeerDTO(Integer id, String name, String ingredients, String alcoholContent, Number price, String category) {
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.alcoholContent = alcoholContent;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getAlcoholContent() {
+        return alcoholContent;
+    }
+
+    public void setAlcoholContent(String alcoholContent) {
+        this.alcoholContent = alcoholContent;
+    }
+
+    public Number getPrice() {
+        return price;
+    }
+
+    public void setPrice(Number price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
