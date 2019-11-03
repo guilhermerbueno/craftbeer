3d2984b402e63d73370043843b838fb1878c3237
guilhermerbueno <gbueno91@gmail.com>
2019-11-03 18:03:35 -0300

    Improving the repository

--- src/main/java/com/beerhouse/repository/BeerRepository.java
+++ src/main/java/com/beerhouse/repository/BeerRepository.java
@@ -4,11 +4,6 @@ import com.beerhouse.model.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * The interface Beer repository
- *
- * @author Guilherme Rodrigues Bueno
- */
 @Repository
 public interface BeerRepository extends JpaRepository<Beer, Integer> {
 
